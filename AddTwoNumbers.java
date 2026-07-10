03d3a1d63ad60c9907fcd0d6bf7d43c88e4f8c59
Abhinav Yadav <abhiydv178@gmail.com>
2026-07-10 23:47:10 +0530

    Create AddTwoNumbers.java

---
+++ AddTwoNumbers.java
@@ -0,0 +1,28 @@
+
+//  * Definition for singly-linked list.
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode h1;
+        ListNode h2;
+        h1 = l1;
+        h2 = l2;
+        
+    }
+}
