e5dc2aabf1752e683aaa59b61f7638c093a696dc
Abhinav Yadav <abhiydv178@gmail.com>
2026-07-10 23:06:56 +0530

    O(n) time complexity with O(1) space complexity solution

--- RomanToInteger.java
+++ RomanToInteger.java
@@ -1,33 +1,22 @@
 class Solution {
     public int romanToInt(String s) {
-        int result = 0;
-        String sym = "IVXLCDM";
-        int val = new int[]{1,5,10,50,100,500,1000};
-        for(int i =0; i<s.length ; i++){
-            for(int j =0; j<7 ; j++){
-                // if(sym[j] == s[i]){
-                //     if(s[i]=='I' && s[i+1]=='V'){
-                //         result+=4;
-                //     }
-                //     else if(s[i]=='I' && s[i+1]=='X'){
-                //         result+=9;
-                //     }
-                //     else if(s[i]=='X' && s[i+1]=='L'){
-                //         result+=40;
-                //     }
-                //     else if(s[i]=='X' && s[i+1]=='C'){
-                //         result+=90;
-                //     }
-                //     else if(s[i]=='C' && s[i+1]=='D'){
-                //         result+=400;
-                //     }
-                //     else(s[i]=='C' && s[i+1]=='M'){
-                //         result+=900;
-                //     }
-                //     result+=val[j];
-                // }
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        int r =0;
+        for(int i = 0; i < s.length()-1; i++){
+            if(map.get(s.charAt(i)) < map.get(s.charAt(i+1))){
+                r-=map.get(s.charAt(i));
+            }
+            else{
+                r+=map.get(s.charAt(i));
             }
         }
-        return result;
+        return r + map.get(s.charAt(s.length()-1));
     }
 }
