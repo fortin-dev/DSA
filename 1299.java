ec614dc583f73f77215d153e79f026d5635700c5
Abhinav Yadav <abhiydv178@gmail.com>
2026-07-16 23:51:04 +0530

    Update 1299.java

---
+++ 1299.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        if(arr==null) return arr;
+        if(arr.length ==1) return new int[]{-1};
+
+        int n = arr.length;
+        int[] ans = new int[n];
+        for(int i = 0 ; i<n-1 ; i++){
+            // ans[i]= arr[i];
+            for(int j = i+1; j<n ; j++){
+                if(ans[i]<=arr[j]){
+                    ans[i]=arr[j];
+                }
+            }
+        }
+        ans[n-1]=-1;
+        return ans;
+    }
+}
