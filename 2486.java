e8d22deda7b0a21cced35ef40b50b6c2514a975f
Abhinav Yadav <abhiydv178@gmail.com>
2026-07-17 15:29:23 +0530

    Update 2486.java

--- 2486.java
+++ 2486.java
@@ -11,3 +11,18 @@ class Solution {
         return t.length()-j;
     }
 }
+
+class Solution {
+    public int appendCharacters(String s, String t) {
+        int j = 0;
+
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i) == t.charAt(j)){
+                j++;
+                if(j == t.length()) return 0;
+            }
+        }
+
+        return t.length()-j;
+    }
+}
