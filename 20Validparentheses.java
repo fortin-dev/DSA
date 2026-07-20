4b9d3657c88d260d93ba053dd72063418752f99f
Abhinav Yadav <abhiydv178@gmail.com>
2026-07-20 23:43:14 +0530

    O(n)tc & O(n)sc using stack

---
+++ 20Validparentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()==0){
+            return false;
+        }
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i< s.length(); i++){
+            char c = s.charAt(i);
+            if(c=='(' || c=='[' || c=='{'){
+                st.push(c);
+            }
+            else if (!st.isEmpty() && c==')' && st.peek()=='(') st.pop();
+            else if (!st.isEmpty() && c=='}' && st.peek()=='{') st.pop();
+            else if (!st.isEmpty() && c==']' && st.peek()=='[') st.pop();
+            else return false;
+        }
+        return st.isEmpty();
+    }
+}
