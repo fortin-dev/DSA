fcc4d64a9278cc7be89a30359851dd56e81b46d2
Abhinav Yadav <abhiydv178@gmail.com>
2026-07-21 13:41:26 +0530

    best approach to get O(1)tc for each fuction call

--- 155MinStack.java
+++ 155MinStack.java
@@ -39,3 +39,50 @@ class MinStack {
         return min;
     }
 }
+// best approach to get O(1)tc for each function call, 
+// but increases sc from O(n) to O(n+n)---- using two stack
+class MinStack {
+    ArrayList<Integer> stack = new ArrayList<>();
+    ArrayList<Integer> mstack = new ArrayList<>();
+    int top;
+    int bottom;
+    int min;
+    public MinStack() {
+        top = -1;
+        bottom = -1;
+        min = 0;
+    }
+
+    public void push(int val) {
+        stack.add(val);
+        int cmin;
+        if (!mstack.isEmpty()) {
+            cmin = mstack.get(top);
+        } else {
+            cmin = val;
+        }
+        val = Math.min(cmin, val);
+        mstack.add(val);
+        top++;
+    }
+
+    public void pop() {
+        if (!stack.isEmpty()) {
+            stack.remove(top);
+            mstack.remove(top);
+            top--;
+        }
+    }
+
+    public int top() {
+        if (!stack.isEmpty()) {
+            return stack.get(top);
+        } else {
+            return 0;
+        }
+    }
+
+    public int getMin() {
+        return mstack.get(top);
+    }
+}
