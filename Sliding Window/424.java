fa1023ad662a6f33093122c04b5adc4389078ac5
Abhinav Yadav <abhiydv178@gmail.com>
2026-07-25 00:39:19 +0530

    more optimal approach of sliding window

--- Sliding Window/424.java
+++ Sliding Window/424.java
@@ -24,3 +24,23 @@ class Solution {
         return max;
     }
 }
+
+//more optimal appraoch of sliding window
+
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int max = 0;
+        int l = 0, maxf = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int r = 0; r < s.length(); r++) {
+            map.put(s.charAt(r), map.getOrDefault(s.charAt(r), 0) + 1);
+            maxf = Math.max(maxf, map.get(s.charAt(r)));
+            while ((r - l + 1) - maxf > k) {
+                map.put(s.charAt(l), map.get(s.charAt(l)) - 1);
+                l++;
+            }
+            max = Math.max(max, r - l + 1);
+        }
+        return max;
+    }
+}
