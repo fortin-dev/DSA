943a72619f7e15871491e53af38d79e9128e4a88
Abhinav Yadav <abhiydv178@gmail.com>
2026-07-26 23:34:02 +0530

    more efficient code
    sliding window approach using array of size 128 (all printable ascii) O(m+n)tc - but more efficient

--- Sliding Window/76.java
+++ Sliding Window/76.java
@@ -42,3 +42,34 @@ public class Solution {
     }
 }
 
+//sliding window approach using array of size 128 (all printable ascii) O(m+n)tc - but more effcient
+class Solution {
+    public String minWindow(String s, String t) {
+        int[] need = new int[128];
+        int[] window = new int[128];
+        for (char c : t.toCharArray()) {
+            ++need[c];
+        }
+        int m = s.length(), n = t.length();
+        int k = -1, winL = m + 1, count = 0;
+        for (int l = 0, r = 0; r < m; ++r) {
+            char c = s.charAt(r);
+            if (++window[c] <= need[c]) {
+                ++count;
+            }
+            while (count == n) {
+                if (r - l + 1 < winL) {
+                    winL = r - l + 1;
+                    k = l;
+                }
+                c = s.charAt(l);
+                if (window[c] <= need[c]) {
+                    --count;
+                }
+                --window[c];
+                ++l;
+            }
+        }
+        return k < 0 ? "" : s.substring(k, k + winL);
+    }
+}
