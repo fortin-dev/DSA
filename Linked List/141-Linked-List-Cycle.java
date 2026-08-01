f29ff5670412f193ceb831c5cd27c9126bd4c655
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-01 21:35:38 +0530

    Linked List Cycle using two pointer

--- Linked List/141-Linked-List-Cycle.java
+++ Linked List/141-Linked-List-Cycle.java
@@ -32,3 +32,20 @@ class Solution {
         return false;
     }
 }
+
+// -----------Using Two pointer-----------
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
