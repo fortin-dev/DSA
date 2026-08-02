75ac3f6953a00ac10f8ee8082dfd961849627c00
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-02 10:54:33 +0530

    Create 143-Reroder-List.java

---
+++ Linked List/143-Reroder-List.java
@@ -0,0 +1,24 @@
+// 143. Reorder List
+// Medium
+// Topics - Linked List, Two Pointers, Stack, Recursion.
+// Companies- LinkedIn
+// You are given the head of a singly linked-list. The list can be represented as:
+
+// L0 → L1 → … → Ln - 1 → Ln
+// Reorder the list to be on the following form:
+
+// L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+// You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+
+
