9b40b04735e2495d9e823d100c26455eabcb1494
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-02 11:23:00 +0530

    Reverse and Merge Approach

--- Linked List/143-Reroder-List.java
+++ Linked List/143-Reroder-List.java
@@ -20,5 +20,36 @@
  * }
  */
 
+//------------------------------Reverse and Merge Approach --------------------
 
+class Solution {
+    public void reorderList(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode second = slow.next;
+        ListNode prev = null;
+        slow.next = null;
+        while (second != null) {
+            ListNode temp = second.next;
+            second.next = prev;
+            prev = second;
+            second = temp;
+        }
+        ListNode first = head;
+        second = prev;
+        while (second!= null) {
+            ListNode t1 = first.next;
+            ListNode t2 = second.next;
+            first.next = second;
+            second.next = t1;
+            first = t1;
+            second = t2;
+        }
+    }
+}
 
