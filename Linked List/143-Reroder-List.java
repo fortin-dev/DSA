43a672a1e2ffb4b5912890b5806c9740352e4e41
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-02 20:20:09 +0530

    Using brute force - O(n)tc with O(n)sc

--- Linked List/143-Reroder-List.java
+++ Linked List/143-Reroder-List.java
@@ -20,7 +20,7 @@
  * }
  */
 
-//------------------------------Reverse and Merge Approach --------------------
+//------------------------------Reverse and Merge Approach- O(n)tc--------------------
 
 class Solution {
     public void reorderList(ListNode head) {
@@ -53,3 +53,30 @@ class Solution {
     }
 }
 
+//--------Using brute force - O(n)tc with O(n)sc 
+class Solution {
+    public void reorderList(ListNode head) {
+        if(head == null){
+            return;
+        }
+        List<ListNode> nodes = new ArrayList<>();
+        ListNode cur = head;
+        while(cur != null){
+            nodes.add(cur);
+            cur = cur.next;
+        }
+
+        int i =0 , j = nodes.size()-1;
+
+        while(i<j){
+            nodes.get(i).next = nodes.get(j);
+            i++;
+            if(i>= j){
+                break;
+            }
+            nodes.get(j).next = nodes.get(i);
+            j--;
+        }
+        nodes.get(i).next = null;
+    }
+}
