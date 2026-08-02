1e2f1d3d59f3b03c758e9cef266d533c1e8bb6f7
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-02 21:20:29 +0530

    Iteration-Two Pass - O(N)tc

--- Linked List/19-Remove-NthNode-From-End.java
+++ Linked List/19-Remove-NthNode-From-End.java
@@ -31,3 +31,27 @@ class Solution {
         return head;
     }
 }
+//--------Iteration-Two Pass - O(N)tc
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int N = 0;
+        ListNode cur = head;
+        while(cur != null){
+            N++;
+            cur = cur.next;
+        }
+        int removeIndex  = N-n;
+        if(removeIndex == 0){
+            return head.next;
+        }
+        cur = head;
+        for(int i =0; i<N-1 ; i++){
+            if(i+1 == removeIndex ){
+                cur.next = cur.next.next;
+                break;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+}
