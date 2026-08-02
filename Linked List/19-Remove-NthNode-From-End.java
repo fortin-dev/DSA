d9033f78c30d1e1548dee78b0dd3d120e22bc432
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-02 23:56:40 +0530

    Using two pointer - O(n)tc class Solution {

--- Linked List/19-Remove-NthNode-From-End.java
+++ Linked List/19-Remove-NthNode-From-End.java
@@ -55,3 +55,21 @@ class Solution {
         return head;
     }
 }
+// ------best Approach - Using two pointer - O(n)tc
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode left = dummy;
+        ListNode right = head;
+        while(n>0){
+            right = right.next;
+            n--;
+        }
+        while(right != null){
+            left = left.next;
+            right = right.next;
+        }
+        left.next = left.next.next;
+        return dummy.next;
+    }
+}
