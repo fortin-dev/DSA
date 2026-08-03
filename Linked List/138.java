20cd90c4f5816dce97ec3fdf054aa40241e0a001
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-03 21:57:41 +0530

    Update 138.java

--- Linked List/138.java
+++ Linked List/138.java
@@ -16,3 +16,40 @@ val: an integer representing Node.val
 random_index: the index of the node (range from 0 to n-1) that the random pointer points to, or null if it does not point to any node.
 Your code will only be given the head of the original linked list.
 */
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        Map<Node, Node> oldToCopy = new HashMap<>();
+        oldToCopy.put(null, null);
+
+        Node cur = head;
+        while(cur != null){
+            Node copy = new Node(cur.val);
+            oldToCopy.put(cur, copy);
+            cur = cur.next;
+        }
+        cur = head;
+        while (cur != null) {
+            Node copy = oldToCopy.get(cur);
+            copy.next = oldToCopy.get(cur.next);
+            copy.random = oldToCopy.get(cur.random);
+            cur = cur.next;
+        }
+
+        return oldToCopy.get(head);
+    }
+}
