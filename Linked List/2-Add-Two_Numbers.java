d9a797ab3723328897196249878cfa86cd01b16c
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-04 20:13:03 +0530

    Create 2-Add-Two_Numbers.java

---
+++ Linked List/2-Add-Two_Numbers.java
@@ -0,0 +1,33 @@
+// 2. Add Two Numbers
+// Medium
+// Topics - Principal, Linked List, Math, Recursion
+// Companies - Amazon
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+//  * Definition for singly-linked list.
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode h1;
+        ListNode h2;
+        h1 = l1;
+        h2 = l2;
+        
+    }
+}
