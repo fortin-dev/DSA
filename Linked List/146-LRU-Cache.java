6881ef6689f6e535a76b1e17efc5ac22b71095ff
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-05 22:26:26 +0530

    Update 146-LRU-Cache.java

--- Linked List/146-LRU-Cache.java
+++ Linked List/146-LRU-Cache.java
@@ -11,3 +11,81 @@ A key is considered used if a get or a put operation is called on it.
 
 Ensure that get and put each run in O(1) average time complexity.
 */
+class LRUCache {
+    Node head, tail;
+    class Node{
+        int key;
+        int val;
+        Node next;
+        Node prev;
+    }
+
+    public void addNode(Node n){
+        n.next = head.next;
+        n.prev = head;
+
+        head.next.prev = n;
+        head.next = n;
+    }
+
+    public void removeNode(Node n){
+        n.prev.next = n.next;
+        n.next.prev = n.prev;
+    }
+
+    public void moveToHead(Node n){
+        removeNode(n);
+        addNode(n);
+    }
+
+    public Node popTail(){
+        Node n = tail.prev;
+        removeNode(n);
+        return n;
+    }
+
+    Map<Integer, Node> map = new HashMap();
+    int capacity;
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        head = new Node();
+        tail = new Node();
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    public int get(int key) {
+        Node n = map.get(key);
+        if(n == null) return -1;
+        moveToHead(n);
+        return n.val;
+    }
+    
+    public void put(int key, int value) {
+        Node n = map.get(key);
+        if(n != null){
+            n.val = value;
+            moveToHead(n);
+        } else {
+            Node node = new Node();
+            node.key = key;
+            node.val = value;
+
+            if(map.size() >= capacity){
+                Node tail = popTail();
+                map.remove(tail.key);
+            }
+
+            map.put(key, node);
+            addNode(node);
+        }
+    }
+}
+
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
