cf6ccefc20e5c3b22c2b49b80385d8a973a836c1
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-07 10:27:41 +0530

    using recursive DFS

--- Trees/226-Invert-BT.java
+++ Trees/226-Invert-BT.java
@@ -10,3 +10,32 @@
     Input: root = [1,2,3,4,5,6,7]
     Output: [1,3,2,7,6,5,4]
 */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
