cc3f4d8f0e5564edec99a4883e75ef63f874e861
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-08 16:06:39 +0530

    Create 543-Diameter-of-BT.java

---
+++ Trees/543-Diameter-of-BT.java
@@ -0,0 +1,32 @@
+/*
+Diameter of Binary Tree
+Easy
+Topics - Tree, DFS, BT
+The diameter of a binary tree is defined as the length of the longest path between any two nodes within the tree. The path does not necessarily have to pass through the root.
+The length of a path between two nodes in a binary tree is the number of edges between the nodes. Note that the path can not include the same node twice
+Given the root of a binary tree root, return the diameter of the tree.
+Example 1:
+Input: root = [1,null,2,3,4,5]
+Output: 3
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        
+    }
+}
