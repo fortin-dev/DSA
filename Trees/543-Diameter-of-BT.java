1d7c8913ea27b8d49401e01b0d894a3cfeffaf41
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-08 20:01:07 +0530

    Using DFS - O(n)tc & O(h)sc

--- Trees/543-Diameter-of-BT.java
+++ Trees/543-Diameter-of-BT.java
@@ -25,8 +25,22 @@ Output: 3
  * }
  */
 
+// Using DFS - O(n)tc & O(h)sc - where n & h are no. of nodes and height of the tree respectively
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
-        
+        if(root == null){
+            return 0;
+        }
+        int leftHeight = maxHeight(root.left);
+        int rightHeight = maxHeight(root.right);
+        int diameter = leftHeight + rightHeight;
+        int sub = Math.max(diameterOfBinaryTree(root.left),diameterOfBinaryTree(root.right));
+        return Math.max(diameter, sub);
+    }
+    public int maxHeight(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        return 1+Math.max(maxHeight(root.left), maxHeight(root.right));
     }
 }
