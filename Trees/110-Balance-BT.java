cbd260e2874e252ae6047b7e974171e9e5e563b4
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-12 18:56:42 +0530

    Create 110-Balance-BT.java

---
+++ Trees/110-Balance-BT.java
@@ -0,0 +1,25 @@
+/*
+    Balanced Binary Tree
+    Easy
+    Topics - Tree, DFS, BT
+    Company Tags - Amazone
+    Given a binary tree, return true if it is height-balanced and false otherwise.
+
+    A height-balanced binary tree is defined as a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
