38a9b5a9183e9ffa824464045923522926b832b4
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-13 20:38:17 +0530

    Create 572-SubTree-of-Another-Tree.java

---
+++ Trees/572-SubTree-of-Another-Tree.java
@@ -0,0 +1,31 @@
+/*
+    Same Binary Tree
+    Easy
+    Topics - Tree, Depth-First Search, String Matching, Binary Tree, Hash Function
+    Company Tags - Amazon
+    Given the roots of two binary trees p and q, return true if the trees are equivalent, otherwise return false.
+    Two binary trees are considered equivalent if they share the exact same structure and the nodes have the same values.
+    Example 1:
+    Input: p = [1,2,3], q = [1,2,3]
+    Output: true
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        
+    }
+}
