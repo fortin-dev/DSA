71ad13eca5f7b6883224af47254257aa6a0ec770
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-14 10:36:40 +0530

    Using Recursion - O(h)tc & sc

--- Trees/235-LCA-BST.java
+++ Trees/235-LCA-BST.java
@@ -26,9 +26,21 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-
-class Solution {
+// Using Recursion - O(h)tc & sc - h is height of tree
+lass Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        
+        if(p.val < root.val && q.val < root.val){
+            return lowestCommonAncestor(root.left , p , q);
+        }
+        if(p.val > root.val && q.val > root.val){
+            return lowestCommonAncestor(root.right , p , q);
+        }
+        if(p.val == root.val || q.val == root.val){
+            return p.val == root.val ? p : q;
+        }
+        if((p.val > root.val && q.val < root.val) || (p.val < root.val && q.val > root.val)){
+            return root;
+        }
+        return root;
     }
 }
