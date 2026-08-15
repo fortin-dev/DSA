383730c2a6d749f9c974f9fbe94ed7a1c7546c93
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-15 09:23:11 +0530

    Create 199-BT-Right-Sice-View.java

---
+++ Trees/199-BT-Right-Sice-View.java
@@ -0,0 +1,28 @@
+/*
+    Binary Tree Right Side View
+    Medium
+    Topics - Tree , DFS, BFS, BT
+    Company Tags- Facebook
+    You are given the root of a binary tree. Return only the values of the nodes that are visible from the right side of the tree, ordered from top to bottom.
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        
+    }
+}
