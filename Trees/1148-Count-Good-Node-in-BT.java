b88bd0d980504acdd83ac9aaac2568e320a17099
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-16 09:51:43 +0530

    Create 1148-Count-Good-Node-in-BT.java

---
+++ Trees/1148-Count-Good-Node-in-BT.java
@@ -0,0 +1,30 @@
+/*
+    Count Good Nodes in Binary Tree
+    Medium
+    Topics
+    Company Tags
+    Hints
+    Within a binary tree, a node x is considered good if the path from the root of the tree to the node x contains no nodes with a value greater than the value of node x
+
+    Given the root of a binary tree root, return the number of good nodes within the tree.
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int goodNodes(TreeNode root) {
+        
+    }
+}
