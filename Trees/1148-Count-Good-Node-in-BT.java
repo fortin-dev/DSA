45975d67f46fc38e2a5c70322a786118949e3974
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-16 10:03:46 +0530

    Using Recursive DFS - O(n)tc & sc

--- Trees/1148-Count-Good-Node-in-BT.java
+++ Trees/1148-Count-Good-Node-in-BT.java
@@ -23,8 +23,22 @@
  *     }
  * }
  */
+// Using Recursive DFS - O(n)tc & sc
 class Solution {
+
     public int goodNodes(TreeNode root) {
-        
+        return dfs(root, root.val);
+    }
+
+    private int dfs(TreeNode node, int maxVal) {
+        if (node == null) {
+            return 0;
+        }
+
+        int count = (node.val >= maxVal) ? 1 : 0;
+        maxVal = Math.max(maxVal, node.val);
+        count += dfs(node.left, maxVal);
+        count += dfs(node.right, maxVal);
+        return count;
     }
 }
