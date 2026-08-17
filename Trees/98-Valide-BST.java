c5f40e9b7bba19c6e7e155fe1b5198d7174c779f
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-17 11:40:39 +0530

    Using iterative DFS

--- Trees/98-Valide-BST.java
+++ Trees/98-Valide-BST.java
@@ -72,3 +72,23 @@ class Solution {
         return true;
     }
 }
+
+// Using iterative DFS - Stack - IN-ORDER TRAVERSAL - O(n)tc & sc
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if(root == null) return true;
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode pre= null;
+        while(root != null || !st.isEmpty()){
+            while(root != null){
+                st.push(root);
+                root = root.left;
+            }
+            root = st.pop();
+            if(pre!=null && root.val <= pre.val) return false;
+            pre = root;
+            root= root.right;
+        }
+        return true;
+    }
+}
