fe9ddd1e5f79d3f9700cfd73bd3e4702279bb997
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-17 12:24:48 +0530

    Using Recursion - DFS

--- Trees/94-BT-In-Order-Traversal.java
+++ Trees/94-BT-In-Order-Traversal.java
@@ -40,3 +40,18 @@ class Solution {
         return res;
     }
 }
+
+// Using Recursion - DFS - O(n)tc & sc
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorder(root, res);
+        return res;
+    }
+    private void inorder(TreeNode node,List<Integer> res ){
+        if(node == null) return;
+        inorder(node.left ,res);
+        res.add(node.val);
+        inorder(node.right ,res);
+    }
+}
