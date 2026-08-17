da7b719cf0d8bbf921f48fdc36efcd8839977ea4
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-17 08:55:14 +0530

    Create 98-Valide-BST.java

---
+++ Trees/98-Valide-BST.java
@@ -0,0 +1,34 @@
+/*
+    98. Validate Binary Search Tree
+    Medium
+    Topics - Tree, BST, DFS, BFS
+    premium lock icon
+    Companies
+    Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+    A valid BST is defined as follows:
+
+    The left subtree of a node contains only nodes with keys strictly less than the node's key.
+    The right subtree of a node contains only nodes with keys strictly greater than the node's key.
+    Both the left and right subtrees must also be binary search trees.
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        
+    }
+}
