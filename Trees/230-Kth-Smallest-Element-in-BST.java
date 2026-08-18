87809af52d8d9105d0655f482e60fe4fb938ca7b
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-18 14:33:16 +0530

    Create 230-Kth-Smallest-Element-in-BST.java

---
+++ Trees/230-Kth-Smallest-Element-in-BST.java
@@ -0,0 +1,32 @@
+/*
+    Kth Smallest Integer in BST
+    Medium
+    Company Tags- Amazone
+    Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) in the tree.
+
+    A binary search tree satisfies the following constraints:
+
+    The left subtree of every node contains only nodes with keys less than the node's key.
+    The right subtree of every node contains only nodes with keys greater than the node's key.
+    Both the left and right subtrees are also binary search trees.
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        
+    }
+}
