aa956ee11479b38610de0ed2ae2e66f8b8239c20
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-19 08:47:41 +0530

    Create 105-ContructTree-From-In&Pre-order.java

---
+++ Trees/105-ContructTree-From-In&Pre-order.java
@@ -0,0 +1,28 @@
+/*
+    105. Construct Binary Tree from Preorder and Inorder Traversal
+    Medium
+    Topics
+    premium lock icon
+    Companies
+    Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        
+    }
+}
