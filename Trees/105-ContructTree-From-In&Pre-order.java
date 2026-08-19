ac1d9a3f2c54157a5cbfbd015db3d8a461f555f6
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-19 08:49:36 +0530

    Update 105-ContructTree-From-In&Pre-order.java

--- Trees/105-ContructTree-From-In&Pre-order.java
+++ Trees/105-ContructTree-From-In&Pre-order.java
@@ -22,7 +22,21 @@
  * }
  */
 class Solution {
+    int preIdx = 0;
+    HashMap<Integer, Integer> indices = new HashMap<>();
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        
+        for(int i = 0 ; i < inorder.length ; i++){
+            indices.put(inorder[i], i);
+        }
+        return dfs(preorder, 0 , inorder.length -1);
+    }
+    private TreeNode dfs(int[] preorder , int l , int r){
+        if(l>r) return null;
+        int rootVal = preorder[preIdx++];
+        TreeNode root = new TreeNode(rootVal);
+        int mid = indices.get(rootVal);
+        root.left = dfs(preorder, l , mid-1);
+        root.right = dfs(preorder, mid+1 , r);
+        return root;
     }
 }
