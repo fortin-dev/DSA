0f6322e5b8170bfbe6bf588e5e92ce3e1fd8e0a4
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-20 16:02:30 +0530

    Create 124-BT-Max-Path.java

---
+++ Trees/124-BT-Max-Path.java
@@ -0,0 +1,32 @@
+/*
+    Binary Tree Maximum Path Sum
+    Hard
+    Topics
+    Company Tags
+    Hints
+    Given the root of a non-empty binary tree, return the maximum path sum of any non-empty path.
+
+    A path in a binary tree is a sequence of nodes where each pair of adjacent nodes has an edge connecting them. A node can not appear in the sequence more than once. The path does not necessarily need to include the root.
+
+    The path sum of a path is the sum of the node's values in the path.
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxPathSum(TreeNode root) {
+        
+    }
+}
