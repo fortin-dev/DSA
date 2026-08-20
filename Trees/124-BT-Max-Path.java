02b8180f15ebd31c939b85cad96380e3e504bbc5
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-20 16:06:33 +0530

    Update 124-BT-Max-Path.java

--- Trees/124-BT-Max-Path.java
+++ Trees/124-BT-Max-Path.java
@@ -27,6 +27,18 @@
  */
 class Solution {
     public int maxPathSum(TreeNode root) {
-        
+        int[] res = new int[] {root.val};
+        dfs(root, res);
+        return res[0];
+    }
+    private int dfs(TreeNode root, int[] res) {
+        if (root == null)
+            return 0;
+
+        int leftMax = Math.max(dfs(root.left, res), 0);
+        int rightMax = Math.max(dfs(root.right, res), 0);
+
+        res[0] = Math.max(res[0], root.val + leftMax + rightMax);
+        return root.val + Math.max(leftMax, rightMax);
     }
 }
