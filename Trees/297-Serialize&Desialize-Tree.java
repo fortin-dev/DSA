82abae5e0911cfa322f4dec91cf98b196393c4c5
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-21 22:32:32 +0530

    Create 297-Serialize&Desialize-Tree.java

---
+++ Trees/297-Serialize&Desialize-Tree.java
@@ -0,0 +1,43 @@
+/*
+    Serialize and Deserialize Binary Tree
+    Hard
+    Topics
+    Company Tags
+    Hints
+    Implement an algorithm to serialize and deserialize a binary tree.
+
+    Serialization is the process of converting an in-memory structure into a sequence of bits so that it can be stored or sent across a network to be reconstructed later in another computer environment.
+
+    You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure. There is no additional restriction on how your serialization/deserialization algorithm should work.
+
+    Note: The input/output format in the examples is the sam
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        
+    }
+}
