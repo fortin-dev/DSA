33405b28ec9d0d5e0c3459401a49492021cd1e55
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-24 09:37:59 +0530

    Update 3622-CheckDivbyDigitSumAndProduct.java

--- Math/3622-CheckDivbyDigitSumAndProduct.java
+++ Math/3622-CheckDivbyDigitSumAndProduct.java
@@ -14,3 +14,16 @@
     Input: n = 99
     Output: true
 */
+class Solution {
+    public boolean checkDivisibility(int n) {
+        int num = n;
+        int sum = 0;
+        int product = 1;
+        while(num > 0){
+            sum+=num%10;
+            product*=num%10;
+            num=num/10;
+        }
+        return n%(sum+product) == 0; 
+    }
+}
