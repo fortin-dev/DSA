396a70289e5597debc3ad5fba22e432dc2224874
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-24 09:37:41 +0530

    Create 3622-CheckDivbyDigitSumAndProduct.java

---
+++ Math/3622-CheckDivbyDigitSumAndProduct.java
@@ -0,0 +1,16 @@
+/*
+    3622. Check Divisibility by Digit Sum and Product
+    Solved
+    Easy
+    Topics
+    premium lock icon
+    Companies
+    Hint
+    You are given a positive integer n. Determine whether n is divisible by the sum of the following two values:
+    The digit sum of n (the sum of its digits).
+    The digit product of n (the product of its digits).
+    Return true if n is divisible by this sum; otherwise, return false.
+    Example 1:
+    Input: n = 99
+    Output: true
+*/
