a128d2d88cfe35b5a833d4ec79bad6666aa9c6d1
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-25 20:01:53 +0530

    Update 58-Length-Of-Last-Word.java

--- String & Characters/58-Length-Of-Last-Word.java
+++ String & Characters/58-Length-Of-Last-Word.java
@@ -27,3 +27,18 @@ class Solution {
         return res;
     }
 }
+
+// More optimise, scanning in reverse order, only calculating length of last word
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int end = s.length() - 1;
+        while (end >= 0 && s.charAt(end) == ' ') {
+            end--;
+        }
+        int start = end;
+        while (start >= 0 && s.charAt(start) != ' ') {
+            start--;
+        }
+        return end - start;        
+    }
+}
