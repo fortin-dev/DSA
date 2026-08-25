c46def42650ab92b6aa69be5d9c3a59c0fd46b9d
Abhinav Yadav <abhiydv178@gmail.com>
2026-08-25 19:59:30 +0530

    Update 58-Length-Of-Last-Word.java

--- String & Characters/58-Length-Of-Last-Word.java
+++ String & Characters/58-Length-Of-Last-Word.java
@@ -5,3 +5,25 @@
     Given a string s consisting of words and spaces, return the length of the last word in the string.
     A word is a maximal substring consisting of non-space characters only.
 */
+// Scanning entire String and updating length based on '_' found
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int n = s.length();
+        int i = 0;
+        int res = 0;
+        while (i < n) {
+            int len = 0;
+            int j = i;
+            if (Character.isWhitespace(s.charAt(i))) {
+                i++;
+            } else {
+                while (i < n && !Character.isWhitespace(s.charAt(i))) {
+                    len++;
+                    i++;
+                }
+                res = len;
+            }
+        }
+        return res;
+    }
+}
