830a64dcb55ff24cc3e20921209810a85f5d926d
Abhinav Yadav <abhiydv178@gmail.com>
2026-09-16 17:19:46 +0530

    Using itereation : no extra space : O(n*2^n)tc

--- Backtracking/90-Subset-II.java
+++ Backtracking/90-Subset-II.java
@@ -56,3 +56,23 @@ public class Solution {
     }
 }
 
+// Using itereation : no extra space : O(n*2^n)tc & O(2^n)sc for output list
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+        int pIdx = 0;
+        int idx =0;
+        for(int i =0 ; i<nums.length ; i++){
+            idx = (i>=1 && nums[i]== nums[i-1]) ? pIdx : 0;
+            pIdx = res.size();
+            for(int j = idx ; j < pIdx; j++){
+                List<Integer> tmp = new ArrayList<>(res.get(j));
+                tmp.add(nums[i]);
+                res.add(tmp);
+            }
+        }
+        return res;
+    }
+}
