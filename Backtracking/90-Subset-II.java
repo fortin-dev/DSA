26b24e0d3da458c6a4195145cdd803b145aa9201
Abhinav Yadav <abhiydv178@gmail.com>
2026-09-16 17:06:23 +0530

    Using Backtracking with extra space : O(n*2^n)tc & O(n)+O(2^n)sc

--- Backtracking/90-Subset-II.java
+++ Backtracking/90-Subset-II.java
@@ -29,3 +29,30 @@ class Solution {
         subset(idx + 1, nums, sset);
     }
 }
+// Using Backtracking with extra space : O(n*2^n)tc & O(n)+O(2^n)sc
+public class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        backtrack(0, new ArrayList<>(), nums);
+        return res;
+    }
+
+    private void backtrack(int i, List<Integer> subset, int[] nums) {
+        if (i == nums.length) {
+            res.add(new ArrayList<>(subset));
+            return;
+        }
+
+        subset.add(nums[i]);
+        backtrack(i + 1, subset, nums);
+        subset.remove(subset.size() - 1);
+
+        while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
+            i++;
+        }
+        backtrack(i + 1, subset, nums);
+    }
+}
+
