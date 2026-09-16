27c46c2af941f3406aacceb7ed7794f97674bafc
Abhinav Yadav <abhiydv178@gmail.com>
2026-09-16 15:15:12 +0530

    Using Backtracking

--- Backtracking/46-Permutation.java
+++ Backtracking/46-Permutation.java
@@ -7,7 +7,7 @@
     Input: nums = [1,2,3]
     Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
 */
-// O(n! * n^2)TC & O(n!*n)SC for Output list.
+// Using iteration O(n! * n^2)TC & O(n!*n)SC for Output list.
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> perms = new ArrayList<>();
@@ -25,5 +25,30 @@ class Solution {
             perms = new_perms;
         }
         return perms;
+    }
+}
+// Using Backtracking : O(n! *n)TC - Less Optimised due to boolean freq array
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        boolean[] freq = new boolean[nums.length];
+        perm(nums, ds , res , freq);
+        return res;
+    }
+    private void perm(int[] nums, List<Integer> ds, List<List<Integer>> res , boolean[] freq){
+        if(ds.size()== nums.length){
+            res.add(new ArrayList<>(ds));
+            return;
+        }
+        for(int i = 0; i<nums.length; i++){
+            if(!freq[i]){
+                freq[i]=true;
+                ds.add(nums[i]);
+                perm(nums, ds , res , freq);
+                ds.remove(ds.size()-1);
+                freq[i]=false;
+            }
+        }
     }
 }
