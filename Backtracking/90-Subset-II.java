470f35ad8a1014714d6a51f2ee057781f755bff8
Abhinav Yadav <abhiydv178@gmail.com>
2026-09-16 16:53:57 +0530

    Create 90-Subset-II.java

---
+++ Backtracking/90-Subset-II.java
@@ -0,0 +1,16 @@
+/*
+    Subsets II
+    Medium
+    Topics
+    Company Tags
+    You are given an array nums of integers, which may contain duplicates. Return all possible subsets.
+    The solution must not contain duplicate subsets. You may return the solution in any order.
+    Example 1:
+    Input: nums = [1,2,1]
+    Output: [[],[1],[1,2],[1,1],[1,2,1],[2]]
+*/
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        
+    }
+}
