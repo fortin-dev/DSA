ef11fb1311d3dd5cf4212a5b7e2d1b588ff555b0
Abhinav Yadav <abhiydv178@gmail.com>
2026-09-16 13:21:48 +0530

    O(n! * n^2)TC & O(n!*n)SC for Output list.

--- Backtracking/46-Permutation.java
+++ Backtracking/46-Permutation.java
@@ -7,3 +7,23 @@
     Input: nums = [1,2,3]
     Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
 */
+// O(n! * n^2)TC & O(n!*n)SC for Output list.
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> perms = new ArrayList<>();
+        perms.add(new ArrayList<>());
+
+        for(int num : nums){
+            List<List<Integer>> new_perms = new ArrayList<>();
+            for(List<Integer> p : perms){
+                for(int i =0; i <= p.size(); i++){
+                    List<Integer> p_copy = new ArrayList<>(p);
+                    p_copy.add(i,num);
+                    new_perms.add(p_copy);
+                }
+            }
+            perms = new_perms;
+        }
+        return perms;
+    }
+}
