db733bac6a742eb795267728daf3be38eca05aed
Abhinav Yadav <abhiydv178@gmail.com>
2026-09-17 12:03:44 +0530

    Using Backtracking with boolean visited array

--- Backtracking/79-Word-Search.java
+++ Backtracking/79-Word-Search.java
@@ -46,3 +46,44 @@ public class Solution {
         return res;
     }
 }
+
+// Using Backtracking with boolean visited array : O(m*4^n)tc & O(n)
+public class Solution {
+    private int ROWS, COLS;
+    private boolean[][] visited;
+
+    public boolean exist(char[][] board, String word) {
+        ROWS = board.length;
+        COLS = board[0].length;
+        visited = new boolean[ROWS][COLS];
+
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                if (dfs(board, word, r, c, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int r, int c, int i) {
+        if (i == word.length()) {
+            return true;
+        }
+
+        if (r < 0 || c < 0 || r >= ROWS || c >= COLS ||
+            board[r][c] != word.charAt(i) || visited[r][c]) {
+            return false;
+        }
+
+        visited[r][c] = true;
+        boolean res = dfs(board, word, r + 1, c, i + 1) ||
+                      dfs(board, word, r - 1, c, i + 1) ||
+                      dfs(board, word, r, c + 1, i + 1) ||
+                      dfs(board, word, r, c - 1, i + 1);
+        visited[r][c] = false;
+
+        return res;
+    }
+}
