6defd031d707c8847d409b2793c85b982b7864d1
Abhinav Yadav <abhiydv178@gmail.com>
2026-09-18 15:44:14 +0530

    corrected the time complexity

--- Backtracking/17-Letter-Combi.java
+++ Backtracking/17-Letter-Combi.java
@@ -6,10 +6,12 @@
     Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
     A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
 */
+/*
+    Using Backtrackig : using a array to store character related to a digit (index) eg at index 2- we store "abc"......., we extract the character set mapped to a digit : for every character in that character set : we append it and recurse to next index.
 
-// Using Backtrackig : using a array to store character related to a digit (index) eg at index 2- we store "abc"......., we extract the character set mapped to a digit : for every character in that character set : we append it and recurse to next index.
-
-// TC : O(4^n)(because 7 & 9 have 4 character) & O(n) for extra space
+    TC : O(n*4^n)(because 7 & 9 have 4 character) 
+    SC : O(n) for extra space & O(n*4^n) for output list
+ */
 class Solution {
     List<String> res = new ArrayList<>();
     private String[] digitToChar = {
