405945145df3be22d7ae36522a6f9e4de9875ea6
Abhinav Yadav <abhiydv178@gmail.com>
2026-09-19 12:37:38 +0530

    Using array : Prefix tree

--- Tries/208-Implement-Trie.java
+++ Tries/208-Implement-Trie.java
@@ -14,3 +14,50 @@
     boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
  
 */
+// Using array : Prefix tree with two properite an array of size 26 for its children and a boolean to mark end of word
+
+//O(n)tc & O(t)sc where n is length of string & t is no. of tries created
+class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    boolean isEnd = false;
+}
+class Trie {
+    TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+    
+    public void insert(String word) {
+        TrieNode cur = root;
+        for(char c : word.toCharArray()){
+            int i = c-'a';
+            if(cur.children[i] == null){
+                cur.children[i] = new TrieNode();
+            }
+            cur = cur.children[i];
+        }
+        cur.isEnd = true;
+    }
+    
+    public boolean search(String word) {
+        TrieNode cur = root;
+        for(char c : word.toCharArray()){
+            int i = c-'a';
+            if(cur.children[i] == null) return false;
+            cur = cur.children[i];
+        }
+        return cur.isEnd ;
+        
+    }
+    
+    public boolean startsWith(String prefix) {
+        TrieNode cur = root;
+        for(char c : prefix.toCharArray()){
+            int i = c-'a';
+            if(cur.children[i] == null) return false;
+            cur = cur.children[i];
+        }
+        return true;
+    }
+}
