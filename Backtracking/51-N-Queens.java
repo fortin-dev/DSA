72a8708dd5c15579b3a1f1e5fd060211838f0c2c
Abhinav Yadav <abhiydv178@gmail.com>
2026-09-19 10:37:42 +0530

    Create 51-N-Queens.java

---
+++ Backtracking/51-N-Queens.java
@@ -0,0 +1,17 @@
+/*
+    51. N-Queens
+    Hard
+    Topics
+    Companies
+    The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+    Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
+    Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
+    Example 1:
+    Input: n = 4
+    Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+*/
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        
+    }
+}
