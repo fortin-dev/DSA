cc75f81a9ebc3824adbd38010ae8d61726522e58
Abhinav Yadav <abhiydv178@gmail.com>
2026-09-21 09:55:10 +0530

    Using Sorting

--- Heap-PriorityQueue/703-Kth-Largest-In-Stream.java
+++ Heap-PriorityQueue/703-Kth-Largest-In-Stream.java
@@ -9,3 +9,22 @@
     constructor(int k, int[] nums) Initializes the object given an integer k and the stream of integers nums.
     int add(int val) Adds the integer val to the stream and returns the kth largest integer in the stream.
 */
+//Using Sorting : everytime we insert -> we sort , so the Kth largest element remains at same positioni -> that is (nums.length - k)
+//  O(m*nlogn) tc & O(m + n)sc -> m : no. of call(made to add()) and n : no. of element (curr. size)
+class KthLargest {
+    List<Integer> arr;
+    int K;
+    public KthLargest(int k, int[] nums) {
+        K = k;
+        arr = new ArrayList();
+        for (int i = 0; i < nums.length; i++) {
+            arr.add(nums[i]);
+        }
+    }
+
+    public int add(int val) {
+        arr.add(val);
+        Collections.sort(arr);
+        return arr.get(arr.size() - K);
+    }
+}
