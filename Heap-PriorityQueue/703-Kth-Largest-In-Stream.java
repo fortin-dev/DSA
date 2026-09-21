1d7e6d461385128ed360efa8233634ad1c3d768e
Abhinav Yadav <abhiydv178@gmail.com>
2026-09-21 09:57:42 +0530

    Using Priority Queue/ Min Heap

--- Heap-PriorityQueue/703-Kth-Largest-In-Stream.java
+++ Heap-PriorityQueue/703-Kth-Largest-In-Stream.java
@@ -28,3 +28,28 @@ class KthLargest {
         return arr.get(arr.size() - K);
     }
 }
+
+// Using Priority Queue/ Min Heap : we only maintain K size heap : K largest element so far , min heap keeps smallest element at top ( which will be the kth largest element so far);
+// O(m*log k)tc and O(k)sc
+class KthLargest {
+    private PriorityQueue<Integer> minHeap;
+    private int k;
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.minHeap = new PriorityQueue<>();
+        for(int num : nums){
+            minHeap.offer(num);
+            if(minHeap.size() > k){
+                minHeap.poll();
+            }
+        }
+    }
+    
+    public int add(int val) {
+        minHeap.offer(val);
+        if(minHeap.size() > k){
+            minHeap.poll();
+        }
+        return minHeap.peek();
+    }
+}
