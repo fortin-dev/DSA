30477e3fc1fe414edf7853aceebbf489576447fb
Abhinav Yadav <abhiydv178@gmail.com>
2026-09-22 15:09:12 +0530

    Create 973-K-Closest-Point.java

---
+++ Heap-PriorityQueue/973-K-Closest-Point.java
@@ -0,0 +1,17 @@
+/*
+    973. K Closest Points to Origin
+    Solved
+    Medium
+    Topics
+    Companies
+    Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane and an integer k, return the k closest points to the origin (0, 0).
+
+    The distance between two points on the X-Y plane is the Euclidean distance (i.e., √(x1 - x2)2 + (y1 - y2)2).
+
+    You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in).
+
+    Example 1:
+
+    Input: points = [[1,3],[-2,2]], k = 1
+    Output: [[-2,2]]
+*/
