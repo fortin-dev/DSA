67519828082d8774e36458fd6af2d6e81f0380d5
Abhinav Yadav <abhiydv178@gmail.com>
2026-09-24 12:04:01 +0530

    using heap

--- Heap-PriorityQueue/295-Find-Midean-DataStram.java
+++ Heap-PriorityQueue/295-Find-Midean-DataStram.java
@@ -35,3 +35,40 @@ class MedianFinder {
         }
     }
 }
+
+// Using Heap : dividing the data stram into two part : max & min - one heap store minimum elements(in maxHeap format) and other store elements greater than smallHeap ( in minHeap format)
+// O(m*log n )tc for addNum() & O(1)for findMedian() where m is function call & n is length or array
+public class MedianFinder {
+
+    private Queue<Integer> maxHeap; //small elements - maxHeap
+    private Queue<Integer> minHeap; //large elements - minHeap
+
+    public MedianFinder() {
+        maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        minHeap = new PriorityQueue<>((a, b) -> a - b);
+    }
+
+    public void addNum(int num) {
+        maxHeap.add(num);
+        if (
+            maxHeap.size() - minHeap.size() > 1 ||
+            !minHeap.isEmpty() &&
+            maxHeap.peek() > minHeap.peek()
+        ) {
+            minHeap.add(maxHeap.poll());
+        }
+        if (minHeap.size() - maxHeap.size() > 1) {
+            maxHeap.add(minHeap.poll());
+        }
+    }
+
+    public double findMedian() {
+        if (maxHeap.size() == minHeap.size()) {
+            return (double) (minHeap.peek() + maxHeap.peek()) / 2;
+        } else if (maxHeap.size() > minHeap.size()) {
+            return (double) maxHeap.peek();
+        } else {
+            return (double) minHeap.peek();
+        }
+    }
+}
