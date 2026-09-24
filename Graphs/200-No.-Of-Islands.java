a57ea50a0912b1d084f4f6d493327b1cd33444eb
Abhinav Yadav <abhiydv178@gmail.com>
2026-09-24 13:10:34 +0530

    Create 200-No.-Of-Islands.java

---
+++ Graphs/200-No.-Of-Islands.java
@@ -0,0 +1,14 @@
+/*
+    Number of Islands
+    Medium
+    Topics
+    Company Tags
+    Given a 2D grid grid where '1' represents land and '0' represents water, count and return the number of islands.
+
+    An island is formed by connecting adjacent lands horizontally or vertically and is surrounded by water. You may assume water is surrounding the grid (i.e., all the edges are water).
+*/
+class Solution {
+    public int numIslands(char[][] grid) {
+        
+    }
+}
