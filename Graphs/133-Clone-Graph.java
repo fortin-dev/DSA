86417d7e857078a82af12f2e45065c779ac6c017
Abhinav Yadav <abhiydv178@gmail.com>
2026-09-25 11:33:04 +0530

    Create 133-Clone-Graph.java

---
+++ Graphs/133-Clone-Graph.java
@@ -0,0 +1,34 @@
+/*
+    133. Clone Graph
+    Medium
+    Topics
+    Companies
+    Given a reference of a node in a connected undirected graph.
+    Return a deep copy (clone) of the graph.
+    Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+*/
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        
+    }
+}
