8cb1f3a52b16a2a5b4952041162a404291db5915
Abhinav Yadav <abhiydv178@gmail.com>
2026-09-25 13:45:08 +0530

    using treasure sourced BFS

--- Graphs/286-Walls&Gates.java
+++ Graphs/286-Walls&Gates.java
@@ -24,3 +24,40 @@
     [0,-1,3,4]
 ]
 */
+
+// Multi source BFS : instead of starting from infinity cells, we start from the treasure cell itself, this was our most repearted work become less
+// O(m*n)tc & sc Where m is the number of rows and n is the number of columns in the grid
+public class Solution {
+    public void islandsAndTreasure(int[][] grid) {
+        Queue<int[]> q = new LinkedList<>();
+        int m = grid.length;
+        int n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0) {
+                    q.add(new int[] { i, j });
+                }
+            }
+        }
+        if (q.size() == 0) return;
+
+        int[][] dirs = { { -1, 0 }, { 0, -1 },
+                         { 1, 0 }, { 0, 1 } };
+        while (!q.isEmpty()) {
+            int[] node = q.poll();
+            int row = node[0];
+            int col = node[1];
+            for (int[] dir : dirs) {
+                int r = row + dir[0];
+                int c = col + dir[1];
+                if (r >= m || c >= n || r < 0 ||
+                    c < 0 || grid[r][c] != Integer.MAX_VALUE) {
+                    continue;
+                }
+                q.add(new int[] { r, c });
+
+                grid[r][c] = grid[row][col] + 1;
+            }
+        }
+    }
+}
