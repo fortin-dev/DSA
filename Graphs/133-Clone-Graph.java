e1b764c7cb839f511f98fe8d39e960685e95ca34
Abhinav Yadav <abhiydv178@gmail.com>
2026-09-25 11:42:50 +0530

    Using DFS + HashMap

--- Graphs/133-Clone-Graph.java
+++ Graphs/133-Clone-Graph.java
@@ -27,8 +27,30 @@ class Node {
 }
 */
 
+// Using DFS + HashMap : why use hashmap: because graphs contains cycles , unlike tree : if we use simple dfs like used in tree , then the recursion will stuck in an cycle i.e infinite loop , so to tackel this we are using hasmap : when we see a new node we craete a copy , when we see same node again , we reuse the already created copy of the node 
+// O(V+E)tc where V no. is vertices and E no. of edges
 class Solution {
     public Node cloneGraph(Node node) {
-        
+        Map<Node, Node> nodeMap = new HashMap<>();
+
+        return dfs(node, nodeMap);
+    }
+    private Node dfs(Node node, Map<Node, Node> nodeMap) {
+        if (node == null) {
+            return null;
+        }
+
+        if (nodeMap.containsKey(node)) {
+            return nodeMap.get(node);
+        }
+
+        Node copy = new Node(node.val);
+        nodeMap.put(node, copy);
+
+        for (Node n : node.neighbors) {
+            copy.neighbors.add(dfs(n, nodeMap));
+        }
+
+        return copy;
     }
 }
