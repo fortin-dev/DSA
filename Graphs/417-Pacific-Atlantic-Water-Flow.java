ed79cf185464c23c1550e7eeb3df24b836794307
Abhinav Yadav <abhiydv178@gmail.com>
2026-09-26 23:03:30 +0530

    using dfs

--- Graphs/417-Pacific-Atlantic-Water-Flow.java
+++ Graphs/417-Pacific-Atlantic-Water-Flow.java
@@ -11,8 +11,45 @@
 
     Return a 2D list of grid coordinates result where result[i] = [ri, ci] denotes that rain water can flow from cell (ri, ci) to both the Pacific and Atlantic oceans.
 */
-class Solution {
+// Using DFS : instead of checking every cell , we do it in reverse ie we start from ocean and move back into cells , with rules inverted this way we do not have to check each and every cell
+// O(m*n)tc & sc 
+public class Solution {
+    private int[][] directions = {{1, 0}, {-1, 0},
+                                  {0, 1}, {0, -1}};
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        
+        int ROWS = heights.length, COLS = heights[0].length;
+        boolean[][] pac = new boolean[ROWS][COLS];
+        boolean[][] atl = new boolean[ROWS][COLS];
+
+        for (int c = 0; c < COLS; c++) {
+            dfs(0, c, pac, heights);
+            dfs(ROWS - 1, c, atl, heights);
+        }
+        for (int r = 0; r < ROWS; r++) {
+            dfs(r, 0, pac, heights);
+            dfs(r, COLS - 1, atl, heights);
+        }
+
+        List<List<Integer>> res = new ArrayList<>();
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                if (pac[r][c] && atl[r][c]) {
+                    res.add(Arrays.asList(r, c));
+                }
+            }
+        }
+        return res;
+    }
+
+    private void dfs(int r, int c, boolean[][] ocean, int[][] heights) {
+        ocean[r][c] = true;
+        for (int[] d : directions) {
+            int nr = r + d[0], nc = c + d[1];
+            if (nr >= 0 && nr < heights.length &&
+                nc >= 0 && nc < heights[0].length &&
+                !ocean[nr][nc] && heights[nr][nc] >= heights[r][c]) {
+                dfs(nr, nc, ocean, heights);
+            }
+        }
     }
 }
