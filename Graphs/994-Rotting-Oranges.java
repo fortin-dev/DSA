00293cfbccae09c420592d2f50a30a62a4a6cf87
Abhinav Yadav <abhiydv178@gmail.com>
2026-09-26 11:42:46 +0530

    using BFS : O(m*n)

--- Graphs/994-Rotting-Oranges.java
+++ Graphs/994-Rotting-Oranges.java
@@ -9,8 +9,49 @@
     Every minute, any fresh orange that is 4-directionally adjacent to a rotten orange becomes rotten.
     Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1.
 */
-class Solution {
+
+// Using similar technique as in islands and trasure or walls and gates : 
+// Using BFS : multi-source bfs : all rotten oranges spread at a unit time to their neighbors : each BFS level represent 1 minute
+// O(m*n)tc & sc  where m is no. of rows and n is no. of columns
+public class Solution {
     public int orangesRotting(int[][] grid) {
-        
+        Queue<int[]> q = new ArrayDeque<>();
+        int fresh = 0;
+        int time = 0;
+
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                if (grid[r][c] == 1) {
+                    fresh++;
+                }
+                if (grid[r][c] == 2) {
+                    q.offer(new int[]{r, c});
+                }
+            }
+        }
+
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        while (fresh > 0 && !q.isEmpty()) {
+            int length = q.size();
+            for (int i = 0; i < length; i++) {
+                int[] curr = q.poll();
+                int r = curr[0];
+                int c = curr[1];
+
+                for (int[] dir : directions) {
+                    int row = r + dir[0];
+                    int col = c + dir[1];
+                    if (row >= 0 && row < grid.length &&
+                        col >= 0 && col < grid[0].length &&
+                        grid[row][col] == 1) {
+                        grid[row][col] = 2;
+                        q.offer(new int[]{row, col});
+                        fresh--;
+                    }
+                }
+            }
+            time++;
+        }
+        return fresh == 0 ? time : -1;
     }
 }
